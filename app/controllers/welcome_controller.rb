88b601cd5540d3013715b37e8fd9b5f2ed1c72af
Ashwini Patil <ashwinip7995@gmail.com>
2018-06-29 13:18:32 -0700

    Deleted WelcomeController and switched to ImageController

--- app/controllers/welcome_controller.rb
+++
@@ -1,9 +0,0 @@
-class WelcomeController < ApplicationController
-  def show
-    @image = Image.find(params[:id])
-  end
-
-  def index
-    @images = Image.order('created_at DESC')
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
-  resources :images, only: %i[new create show]
+  root 'images#index'
+  resources :images, only: %i[index new create show]
 end
